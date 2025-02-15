f647e1903371d65056aef4937901bbc5696e951e
Sengtri Bun <sengtri457@gmail.com>
2025-02-15 20:55:36 +0700

    "add more files"

---
+++ Models/UserRoles/RoleManager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MIdTerm_c_.Models.UserRoles
+{
+    internal class RoleManager
+    {
+
+
+    }
+}
